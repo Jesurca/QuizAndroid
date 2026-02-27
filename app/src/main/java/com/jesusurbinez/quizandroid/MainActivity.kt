10766cd9c1ffeab1ee0f2b751c09649c858484ab
Jesurca <Jesurca01@gmail.com>
2026-02-27 17:41:34 -0500

    Primera pantalla: figuras

--- app/src/main/java/com/jesusurbinez/quizandroid/MainActivity.kt
+++ app/src/main/java/com/jesusurbinez/quizandroid/MainActivity.kt
@@ -4,44 +4,118 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jesusurbinez.quizandroid.ui.theme.QuizAndroidJesusUrbinezTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            QuizAndroidJesusUrbinezTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuizAndroidJesusUrbinezTheme {
-        Greeting("Android")
+        @Composable
+        fun ContentScreen() {
+            Column(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceAround
+
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Cyan)
+                        .padding(18.dp), horizontalArrangement = Arrangement.Center
+
+
+                ) {
+                    Text(
+                        text = "item1",
+                        modifier = Modifier
+                            .background(Color.Green)
+                            .padding(8.dp)
+                    )
+
+                    Text(
+                        text = "item2",
+                        modifier = Modifier
+                            .background(Color.Yellow)
+                            .padding(8.dp)
+                    )
+
+                    Text(
+                        text = "item3",
+                        modifier = Modifier
+                            .background(Color.Red)
+                            .padding(8.dp)
+                    )
+
+                }
+                Text(
+                    text = "item4",
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+                Text(
+                    text = "text",
+                    modifier = Modifier
+                        .background(Color.Cyan)
+                        .fillMaxWidth()
+                        .padding(8.dp)
+
+                )
+                Text(
+                    text = "textito",
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .fillMaxWidth()
+                        .padding(8.dp)
+
+                )
+            }
+        }
+
+        @Composable
+
+        fun IDK_C() {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Row(modifier = Modifier.weight(1f)) {
+                    Column(modifier = Modifier.background(Color.Red).weight(1f)) {
+                        Text(text = "item")
+                        Text(text = "item")
+                    }
+                    Column(modifier = Modifier.background(Color.Red).weight(1f)) {
+                        Text(text = "item")
+                        Text(text = "item")
+                    }
+                }
+            }
+        }
+
     }
 }
+
+annotation class ContentScreen
+
